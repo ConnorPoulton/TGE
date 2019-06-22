518e73bef05cdf35e3e36fc70343e430840d54ab
ConnorPoulton <poulton.cw@gmail.com>
2019-06-22 19:09:52 -0400

    Added file structures and added outline for room manager

---
+++ TGE/Assets/Scripts/ICommand.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ICommand : ScriptableObject
+{
+    public abstract void Action(GameObject gameObject);
+}
---
+++ TGE/Assets/Scripts/RoomManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public Room currentRoom;
+
+    public void LoadRoom()
+    {
+
+    }
+
+    public void ChangeRoom(Room newRoom)
+    {
+
+    }
+}
